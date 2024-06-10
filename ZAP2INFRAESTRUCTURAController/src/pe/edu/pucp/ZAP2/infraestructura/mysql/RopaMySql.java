944f29d34b590c44adb2612185fe556e3f438241
ErMo2 <a20190315@pucp.edu.pe>
2024-06-09 19:00:16 -0500

    gogogogo

--- ZAP2INFRAESTRUCTURAController/src/pe/edu/pucp/ZAP2/infraestructura/mysql/RopaMySql.java
+++ ZAP2INFRAESTRUCTURAController/src/pe/edu/pucp/ZAP2/infraestructura/mysql/RopaMySql.java
@@ -4,7 +4,12 @@
  */
 package pe.edu.pucp.ZAP2.infraestructura.mysql;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
+import pe.edu.pucp.ZAP2.DBManager.DBManager;
 import pe.edu.pucp.ZAP2.infraestructura.dao.RopaDao;
 import pe.edu.pucp.ZAP2.infraestructura.model.Ropa;
 
@@ -14,9 +19,30 @@ import pe.edu.pucp.ZAP2.infraestructura.model.Ropa;
  */
 public class RopaMySql implements RopaDao{
 
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    private CallableStatement cs;
     @Override
     public int insertar(Ropa ropa) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int resultado = 0;
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call INSERTAR_ROPA"
+                    +"(?,?,?,?,?)}");
+            cs.registerOutParameter("_id_ropa", java.sql.Types.INTEGER);
+            cs.setString("_nombre", ropa.getNombre());
+            cs.setString("_descripcion", ropa.getDescripcion());
+            cs.setString("_material", ropa.getMaterial());
+            cs.setString("_tipo", ropa.getTipo().toString());
+            resultado = cs.executeUpdate();
+            ropa.setIdProducto(cs.getInt("_id_ropa"));
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){ System.out.println(ex.getMessage());}
+        }
+        return resultado;
     }
 
     @Override
