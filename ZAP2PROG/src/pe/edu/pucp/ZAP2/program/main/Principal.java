6e0c4e8a39938c6f39b208fecb8d03f72bfb5e4a
LeonardoArdo <99337812+misahor67@users.noreply.github.com>
2024-06-09 21:09:54 -0500

    Avanzando

--- ZAP2PROG/src/pe/edu/pucp/ZAP2/program/main/Principal.java
+++ ZAP2PROG/src/pe/edu/pucp/ZAP2/program/main/Principal.java
@@ -14,6 +14,8 @@ import pe.edu.pucp.ZAP2.infraestructura.dao.CajeroDao;
 import pe.edu.pucp.ZAP2.infraestructura.dao.ClienteDao;
 import pe.edu.pucp.ZAP2.infraestructura.dao.CuentaUsuarioDao;
 import pe.edu.pucp.ZAP2.infraestructura.dao.EmpleadoDeAreaDao;
+import pe.edu.pucp.ZAP2.infraestructura.dao.LoteDao;
+import pe.edu.pucp.ZAP2.infraestructura.dao.ProductoPerecibleDao;
 import pe.edu.pucp.ZAP2.infraestructura.dao.SucursalDao;
 import pe.edu.pucp.ZAP2.infraestructura.dao.SupervisorDao;
 import pe.edu.pucp.ZAP2.infraestructura.model.Almacen;
@@ -21,23 +23,35 @@ import pe.edu.pucp.ZAP2.infraestructura.model.Area;
 import pe.edu.pucp.ZAP2.infraestructura.model.Cajero;
 import pe.edu.pucp.ZAP2.infraestructura.model.Cliente;
 import pe.edu.pucp.ZAP2.infraestructura.model.CuentaUsuario;
+import pe.edu.pucp.ZAP2.infraestructura.model.Descuento;
 import pe.edu.pucp.ZAP2.infraestructura.model.EmpleadoDeArea;
 import pe.edu.pucp.ZAP2.infraestructura.model.Lote;
+import pe.edu.pucp.ZAP2.infraestructura.model.Producto;
+import pe.edu.pucp.ZAP2.infraestructura.model.ProductoPerecible;
+import pe.edu.pucp.ZAP2.infraestructura.model.Ropa;
 import pe.edu.pucp.ZAP2.infraestructura.model.Sucursal;
 import pe.edu.pucp.ZAP2.infraestructura.model.Supervisor;
 import pe.edu.pucp.ZAP2.infraestructura.model.TipoAlmacen;
 import pe.edu.pucp.ZAP2.infraestructura.model.TipoContrato;
+import pe.edu.pucp.ZAP2.infraestructura.model.TipoProductoPerecible;
 import pe.edu.pucp.ZAP2.infraestructura.model.TipoPuesto;
 import pe.edu.pucp.ZAP2.infraestructura.model.TurnosHorario;
+import pe.edu.pucp.ZAP2.infraestructura.model.UnidadDeMedida;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.AlmacenMySql;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.AreaMySql;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.CajeroMySql;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.ClienteMySql;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.CuentaUsuarioMySql;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.EmpleadoDeAreaMySql;
+import pe.edu.pucp.ZAP2.infraestructura.mysql.LoteMySql;
+import pe.edu.pucp.ZAP2.infraestructura.mysql.ProductoPerecibleMySql;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.SucursalMySql;
 import pe.edu.pucp.ZAP2.infraestructura.mysql.SupervisorMySql;
 import pe.edu.pucp.ZAP2.personas.model.TipoDocumento;
+import pe.edu.pucp.ZAP2.proveedor.dao.PedidoDao;
+import pe.edu.pucp.ZAP2.proveedor.model.Estado_Pedido;
+import pe.edu.pucp.ZAP2.proveedor.model.Pedido;
+import pe.edu.pucp.ZAP2.proveedor.mysql.PedidoMySql;
 
 /**
  *
@@ -50,9 +64,12 @@ public class Principal {
         /*=============================*/
         /*==== Objetos compartidos ====*/
         /*=============================*/
+        Producto producto = new ProductoPerecible();
+        producto.setIdProducto(1);
+        
         String fechaStr = "2022-05-10"; // Formato de fecha: "yyyy-MM-dd"
         SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-        Date fecha_Contratacion = formato.parse(fechaStr);
+        Date fecha = formato.parse(fechaStr);
         
         Sucursal sucursal = new Sucursal(1, "Av. Pepito Valle",
                 1300.4, "Pepe's shop",
@@ -60,9 +77,11 @@ public class Principal {
         
         Area area = new Area(1, "Zona de alimentos",sucursal, null);
         
-        Supervisor supervisor = new Supervisor(5,null,null, 6, 456.5, fecha_Contratacion, TipoContrato.TiempoCompleto, TurnosHorario.MAÑANA, 
+        Supervisor supervisor = new Supervisor(5,null,null, 6, 456.5, fecha, TipoContrato.TiempoCompleto, TurnosHorario.MAÑANA, 
                 area, 'M', "Mz C Lt9 Señor de los Milagros", null, 6, "Juan", "Vega", "Suares", 456485489, "juan.perez@example.com", 
                 TipoDocumento.DNI, 75607208);
+        
+        Pedido pedido = new Pedido(1, 500.6, Estado_Pedido.COMPLETADO, fecha, 468.8, null);
         
         /*==================*/
         /*==== Sucursal ====*/
@@ -80,9 +99,8 @@ public class Principal {
         /*==== Almacen ====*/
         /*=================*/
 //        Lote lote = new Lote();
-//       
+//        Almacen almacen = new Almacen(1,TipoAlmacen.ALMACENCOMUN,500.0,0.0,null,true,sucursal,lote);
 //        AlmacenDao daoAlmacen = new AlmacenMySql();
-//        Almacen almacen = new Almacen(0,TipoAlmacen.ALMACENCOMUN,500.0,0.0,null,true,sucursal,lote);
 //        daoAlmacen.insertar(almacen);
 //        
         /*==============*/
@@ -105,7 +123,7 @@ public class Principal {
         /*=== Cajero ==*/
         /*=============*/
 //        CajeroDao daoCajero = new CajeroMySql();
-//        Cajero cajero = new Cajero(1, 2, supervisor, 0, 456.5, fecha_Contratacion, TipoContrato.TiempoCompleto, TurnosHorario.MAÑANA, 
+//        Cajero cajero = new Cajero(1, 2, supervisor, 0, 456.5, fecha, TipoContrato.TiempoCompleto, TurnosHorario.MAÑANA, 
 //                area, 'M', "Mz C Lt9 Señor de los Milagros", null, 0, "Juan", "Vega", "Suares", 456485489, "juan.perez@example.com", TipoDocumento.DNI, 75607208);
 //        daoCajero.insertar(cajero);
         
@@ -113,7 +131,7 @@ public class Principal {
         /*=== EmpleadoDeArea ===*/
         /*========================*/
 //        EmpleadoDeAreaDao daoEmpleadoDeArea = new EmpleadoDeAreaMySql();
-//        EmpleadoDeArea empDeArea = new EmpleadoDeArea(supervisor, TipoPuesto.Consultor, 0, 456.5, fecha_Contratacion, TipoContrato.TiempoCompleto, TurnosHorario.MAÑANA, 
+//        EmpleadoDeArea empDeArea = new EmpleadoDeArea(supervisor, TipoPuesto.Consultor, 0, 456.5, fecha, TipoContrato.TiempoCompleto, TurnosHorario.MAÑANA, 
 //                area, 'M', "Mz C Lt9 Señor de los Milagros", null, 0, "Juan", "Vega", "Suares", 456485489, "juan.perez@example.com", 
 //                TipoDocumento.DNI, 75607208);
 //        daoEmpleadoDeArea.insertar(empDeArea);
@@ -125,8 +143,39 @@ public class Principal {
 //        CuentaUsuario cuentaUsu = new CuentaUsuario(0, "jorge123", "paraalo456*4", true, supervisor);
 //        daoCuentaUsu.insertar(cuentaUsu);
     
-                
-                
+        /*========================*/
+        /*========= Pedido =========*/
+        /*========================*/
+//        PedidoDao daoPedido = new PedidoMySql();
+//        daoPedido.insertar(pedido);
+
+        /*=============================*/
+        /*========= Descuento =========*/
+        /*============================*/
+        DescuentoDao daoDescuento = new DescuentoMySql();
+        Descuento descuento = new Descuento(0, 0, 0, fecha, fecha, 0, prodPrecio);
+        daoDescuento.insertar(descuento);
+        /*===================================*/
+        /*========= ProductoPrecio =========*/
+        /*=================================*/
+    
+        /*============================*/
+        /*========= Producto =========*/
+        /*===========================*/
+//        ProductoPerecibleDao daoProdPerec = new ProductoPerecibleMySql();
+//        ProductoPerecible prodPerecible = new ProductoPerecible(fecha, TipoProductoPerecible.CEREALES, UnidadDeMedida.UNIDAD, 
+//                0, fechaStr, fechaStr, null, 0, null);
+//        daoProdPerec.insertar(prodPerecible);
+        
+        /*========================*/
+        /*========= Lote =========*/
+        /*========================*/
+//        Almacen almacen = new Almacen();
+//        almacen.setId_almacen(1);
+//        
+//        LoteDao daoLote = new LoteMySql();
+//        Lote lote = new Lote(1,50 , 20, almacen, producto, null, 1);
+//        daoLote.insertar(lote);
  }
     
 }
