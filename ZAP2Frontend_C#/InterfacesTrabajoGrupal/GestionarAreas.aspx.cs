78128a48d2e550bcb261d72b337e2cfa6c98b253
Coco12-code <126829477+Coco12-code@users.noreply.github.com>
2024-06-13 17:55:18 -0500

    Fixed ListarAreas

--- ZAP2Frontend_C#/InterfacesTrabajoGrupal/GestionarAreas.aspx.cs
+++ ZAP2Frontend_C#/InterfacesTrabajoGrupal/GestionarAreas.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using InterfacesTrabajoGrupal.ServicioWS;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Web;
@@ -10,9 +12,31 @@ namespace InterfacesTrabajoGrupal
 {
     public partial class GestionarAreas : System.Web.UI.Page
     {
+        private AreaWSClient daoArea;
+        private area area;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                int idArea;
+                if (int.TryParse(Request.QueryString["id"], out idArea))
+                {
+                    CargarArea(idArea);
+                }
+            }
+        }
+        private void CargarArea(int idArea)
+        {
+            daoArea = new AreaWSClient();
 
+            area = daoArea.buscarArea(idArea);
+            if (area != null)
+            {
+                txtIdAmbiente.Text = area.idArea.ToString();
+                txtNombreAmbiente.Text = area.nombre;
+                txtSucursal.Text = area.sucursal.id_sucursal.ToString();
+            }
+            
         }
         protected void Page_Init(object sender, EventArgs e)
         {
