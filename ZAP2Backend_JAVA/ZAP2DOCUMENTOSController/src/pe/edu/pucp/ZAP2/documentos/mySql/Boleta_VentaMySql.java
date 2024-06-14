5013f8f5d47df9e133197671f0a042e033eaf09d
Coco12-code <a20182677@pucp.edu.pe>
2024-06-14 12:01:32 -0500

    Arreglado Listar Boleta Venta

--- ZAP2Backend_JAVA/ZAP2DOCUMENTOSController/src/pe/edu/pucp/ZAP2/documentos/mySql/Boleta_VentaMySql.java
+++ ZAP2Backend_JAVA/ZAP2DOCUMENTOSController/src/pe/edu/pucp/ZAP2/documentos/mySql/Boleta_VentaMySql.java
@@ -15,6 +15,7 @@ import pe.edu.pucp.ZAP2.documentos.dao.Boleta_VentaDao;
 import pe.edu.pucp.ZAP2.documentos.model.Boleta_Venta;
 import pe.edu.pucp.ZAP2.documentos.model.Moneda;
 import pe.edu.pucp.ZAP2.documentos.model.Tarjeta;
+import pe.edu.pucp.ZAP2.infraestructura.model.Cajero;
 import pe.edu.pucp.ZAP2.infraestructura.model.Cliente;
 import pe.edu.pucp.ZAP2.infraestructura.model.Empleado;
 import pe.edu.pucp.ZAP2.personas.model.Persona;
@@ -116,7 +117,7 @@ public class Boleta_VentaMySql implements Boleta_VentaDao{
                 Boleta_Venta bolVenta = new Boleta_Venta();
                 bolVenta.setId_doc_venta(rs.getInt("id_boleta_venta"));
                 
-                Empleado empleado = new Empleado(){};
+                Empleado empleado = new Cajero();
                 empleado.setId_Persona(rs.getInt("fid_id_persona"));//Cambiar el fid_id_persona por fid_id_empleado
                 empleado.setIdEmpleado(rs.getInt("fid_id_persona"));//Cambiar el fid_id_persona por fid_id_empleado
                 bolVenta.setEmpleado(empleado);
